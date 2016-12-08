5b126014644c027ac30fcc8646cc6555511cc248
Hissa Massa <hissamassa7@gmail.com>
2016-12-09 01:46:20 +0900

    1レーン音ゲーの見た目を作成

---
+++ Assets/Sources/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+    public GameObject MainCamera;
+    public GameObject NotePrefab;
+    
+    // Use this for initialization
+	void Start () {
+        CreateNote();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void CreateNote()
+    {
+        var camera = MainCamera.GetComponent<Camera>();
+        var point = camera.ScreenToWorldPoint(new Vector2(0, camera.pixelHeight));
+        float topLine = point.y;
+        Instantiate(NotePrefab, new Vector2(0, topLine), Quaternion.identity);
+    }
+}
---
+++ Assets/Sources/Note.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Note : MonoBehaviour {
+
+    public float NoteSpeed = 1;
+    public float LinePosition;
+    public GameObject Effect;
+
+	// Use this for initialization
+	void Start () {
+        var line = GameObject.Find("Line");
+        var lineTransform = line.GetComponent<Transform>();
+        LinePosition = lineTransform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var transform = GetComponent<Transform>();
+        Vector2 now = transform.position;
+        var movedPosition = new Vector2(now.x, now.y - 0.2f);
+        transform.position = movedPosition;
+        if(movedPosition.y <= LinePosition)
+        {
+            Debug.Log("Good");
+            Instantiate(Effect, GetComponent<Transform>().position, Quaternion.identity);
+            Destroy(gameObject);
+            var controller = GameObject.Find("GameController").GetComponent<GameController>();
+            controller.CreateNote();
+        }
+	}
+}
