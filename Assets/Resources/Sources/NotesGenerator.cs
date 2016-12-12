10154965b605d15713db539d0174f8f15fdefa0d
Hissa Massa <hissamassa7@gmail.com>
2016-12-12 18:29:50 +0900

    NotesManagerで譜面を管理するように変更

--- Assets/Resources/Sources/NotesGenerator.cs
+++ Assets/Resources/Sources/NotesGenerator.cs
@@ -10,36 +10,15 @@ public class NotesGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        Notes = new List<float>();
-        Notes.Add(1f);
-        Notes.Add(2f);
-        Notes.Add(3f);
-        Notes.Add(4f);
-        Notes.Add(5f);
-        Notes.Add(5.5f);
-        Notes.Add(6f);
-        Notes.Add(6.5f);
-        Notes.Add(7f);
-        Notes.Add(7.5f);
-        Notes.Add(8f);
-        Notes.Add(8.5f);
-
-        //for(var i = 0; i < Notes.Count; i++)
-        //{
-        //    Notes[i] += 10f;
-        //}
-
-        StartTime = Time.timeSinceLevelLoad;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        var musicTime = Time.timeSinceLevelLoad - StartTime;
-        Debug.Log(musicTime);
-        if (Notes.Count > 0 && Notes[0] <= musicTime)
-        {
-            Instantiate(Note, new Vector2(0, 10000), Quaternion.identity);
-            Notes.RemoveAt(0);
-        }
+	void Update () { 
 	}
+
+    public GameObject GenerateNote()
+    {
+        GameObject createdObject = Instantiate(Note, new Vector2(0, 1000), Quaternion.identity);
+        return createdObject;
+    }
 }
---
+++ Assets/Resources/Sources/NotesManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotesManager : MonoBehaviour {
+    public List<NoteData> Notes;
+    public int ShowCursor;
+    public int JudgeCursor;
+    public float StartTime;
+    public List<GameObject> CreatedNotes;
+    public GameObject NotesGenerator;
+    public NotesGenerator Generator;
+    public float NoteSpeed;
+
+	// Use this for initialization
+	void Start () {
+        NoteSpeed = 1;
+        ShowCursor = 0;
+        JudgeCursor = 0;
+        Generator = NotesGenerator.GetComponent<NotesGenerator>();
+        StartTime = Time.timeSinceLevelLoad;
+        Notes = new List<NoteData>();
+        Notes.Add(new NoteData(1f));
+        Notes.Add(new NoteData(2f));
+        Notes.Add(new NoteData(3f));
+        Notes.Add(new NoteData(4f));
+        CreatedNotes = new List<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var musicTime = Time.timeSinceLevelLoad - StartTime;
+        ShowNote(musicTime);
+	}
+
+    public void ShowNote(float musicTime)
+    {
+        if (ShowCursor < Notes.Count && Notes[ShowCursor].Time - NoteSpeed <= musicTime)
+        {
+            CreatedNotes.Add(Generator.GenerateNote());
+            ShowCursor++;
+        }
+    }
+}
